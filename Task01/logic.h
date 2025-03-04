2352b311a4a97a3dd77f67cc8aeb49e98c31c974
Danila Sosnov <dsosnov2009@gmail.com>
2025-03-04 17:36:51 +0300

    Add check_local_min and check_local_max functions

---
+++ Task01/logic.h
@@ -0,0 +1,3 @@
+#pragma once
+bool check_local_min(int* array, int size);
+bool check_local_max(int* array, int size);
--- Task01/main.cpp
+++ Task01/main.cpp
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "logic.h"
 
 int main()
 {
@@ -12,7 +13,13 @@ int main()
 
 	init(pointer, size, -20, 20);
 
-	cout << "Array: " << convert(pointer, size);
+	cout << "Array: " << convert(pointer, size) << endl;
+
+	string msg_min = check_local_min(pointer, size) ? "Yes." : "No.";
+	string msg_max = check_local_max(pointer, size) ? "Yes." : "No.";
+
+	cout << "Does local min exist? " << msg_min << endl;
+	cout << "Does local max exist? " << msg_max << endl;
 	
 
 	
